388f301c9e487c773251ee904b77f834903e38ee
impronen <impronen@gmail.com>
2024-07-20 19:25:06 +0300

    add dataCopier()

---
+++ scripts/background.js
@@ -0,0 +1,27 @@
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.action.setBadgeText({
+    text: "OFF",
+  });
+});
+
+function dataCopier() {
+  const data = document.querySelectorAll(".ydum_");
+
+  const inputValues = {};
+
+  data.forEach((element) => {
+    const childDivs = element.querySelectorAll("div");
+
+    childDivs.forEach((child) => {
+      const label = child.querySelector("label");
+      const input = child.querySelector('input[type="text"]');
+
+      if (label && input) {
+        const labelText = label.textContent.trim();
+        const inputValue = input.value.trim();
+        inputValues[labelText] = inputValue;
+      }
+    });
+  });
+  return inputValues;
+}
