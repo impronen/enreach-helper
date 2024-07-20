8d4d2f50ab29704f5c7252423dcf4d9975eb4d66
impronen <impronen@gmail.com>
2024-07-20 19:39:58 +0300

    add dataPaster() function

--- scripts/background.js
+++ scripts/background.js
@@ -25,3 +25,25 @@ function dataCopier() {
   });
   return inputValues;
 }
+
+function dataPaster(inputValues) {
+  console.log(inputValues);
+
+  const data = document.querySelectorAll(".ydum_");
+
+  data.forEach((element) => {
+    const childDivs = element.querySelectorAll("div");
+
+    childDivs.forEach((child) => {
+      const label = child.querySelector("label");
+      const input = child.querySelector('input[type="text"]');
+
+      if (label && input) {
+        const labelText = label.textContent.trim();
+        if (inputValues.hasOwnProperty(labelText)) {
+          input.value = inputValues[labelText];
+        }
+      }
+    });
+  });
+}
