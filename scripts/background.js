5f1260c2622c90231b5af887ec1583742fb53134
impronen <impronen@gmail.com>
2024-08-08 16:44:21 +0300

    functionality complete

--- scripts/background.js
+++ scripts/background.js
@@ -1,10 +1,76 @@
+const menuItems = ["Copy from Enreach", "Paste from Enreach"];
+
+// Create context menu items
 chrome.runtime.onInstalled.addListener(() => {
   chrome.action.setBadgeText({
     text: "OFF",
   });
+
+  menuItems.forEach((item) => {
+    chrome.contextMenus.create({
+      id: item,
+      title: item,
+      type: "normal",
+      contexts: ["all"],
+    });
+  });
 });
 
+// Context menu listener
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === "Copy from Enreach") {
+    handleCopyCommand(tab);
+  } else if (info.menuItemId === "Paste from Enreach") {
+    handlePasteCommand(tab);
+  }
+});
+
+// Shortcut listener
+chrome.commands.onCommand.addListener((command) => {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    if (command === "run-copy") {
+      handleCopyCommand(tabs[0]);
+    } else if (command === "run-paste") {
+      handlePasteCommand(tabs[0]);
+    }
+  });
+});
+
+// Extension side copy
+function handleCopyCommand(tab) {
+  chrome.scripting.executeScript(
+    {
+      target: { tabId: tab.id },
+      function: dataCopier,
+    },
+    (results) => {
+      if (results && results.length > 0) {
+        const copiedData = results[0].result;
+        chrome.storage.local.set({ copiedData: copiedData }, () => {
+          console.log("Data copied and stored:", copiedData);
+        });
+      }
+    }
+  );
+}
+
+// Extension side paste
+function handlePasteCommand(tab) {
+  chrome.storage.local.get("copiedData", (result) => {
+    const inputValues = result.copiedData;
+    if (inputValues) {
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: dataPaster,
+        args: [inputValues],
+      });
+    }
+  });
+}
+
+// Actual copying
 function dataCopier() {
+  console.log("Running Copy!");
   const data = document.querySelectorAll(".ydum_");
 
   const inputValues = {};
@@ -23,9 +89,11 @@ function dataCopier() {
       }
     });
   });
+
   return inputValues;
 }
 
+// Actual paste
 function dataPaster(inputValues) {
   console.log(inputValues);
 
